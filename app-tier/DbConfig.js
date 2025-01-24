7d739b8fd78fe97b19c3b7f293e283c8d4f4c75c
Shivendra Kumar Patel <shivendrap455@gmail.com>
2025-01-24 14:20:55 +0530

    Update DbConfig.js

--- app-tier/DbConfig.js
+++ app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'database-1-instance-1.c14u6ikiivvj.us-east-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : '12345678',
-    DB_DATABASE : 'webappdb'
+    DB_HOST : 'Your database endpoint / hostname',
+    DB_USER : 'username',
+    DB_PWD : 'password',
+    DB_DATABASE : 'database name'
 });
